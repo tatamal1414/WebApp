01b7bc8407c77891c497011e752b8ad4c7e73805
tatamal1414 <2004240012nt@edu.oca.ac.jp>
2022-06-18 13:51:22 +0900

    app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,request
+from flask import Flask,request,render_template
 app = Flask(__name__)
-@app.route('/test_request')
-def test_request():
-    return f'test_request:{request.args.get("dummy")}'
+@app.route('/show_html')
+def show_html():
+    return render_template('test_html.html')
