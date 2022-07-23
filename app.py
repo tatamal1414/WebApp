85be23c20acff3986683fd839e005dd40def6f6a
tatamal1414 <2004240012nt@edu.oca.ac.jp>
2022-07-23 17:13:47 +0900

    h

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
 from urllib import response
 from flask import Flask, request, render_template,jsonify
+#from test_model import Person
+from mysql_model import Person
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+
+
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:p%40ssw0rd1@mysqldb/test_mysql?charset=utf8mb4'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+
 
 @app.route("/")
 def index():
@@ -42,4 +57,13 @@ def try_rest():
     return jsonify(response_json)
 
 
+@app.route('/person_search')
+def person_search():
+    return render_template('./person_search.html')
 
+
+@app.route('/person_result')
+def person_result():
+    search_size = request.args.get("search_size")
+    persons = db.session.query(Person).filter(Person.size > search_size)
+    return render_template('./person_result.html', persons=persons, search_size=search_size)
